f029f0d3a7f68d8ed8ce81efc7f51b534444b6ca
Tarik02 <Taras.Fomin@gmail.com>
2026-05-01 10:44:04 +0300

    provide git askpass credentials

--- packages/backend/src/gitlab/provider/client.ts
+++ packages/backend/src/gitlab/provider/client.ts
@@ -1190,6 +1190,10 @@ function makeGitLabProvider(): ForgeProviderEffectContract<GitLabApiClient, GitL
       return {
         url: `${repo.host}/${repo.path}.git`,
         auth: {
+          askPass: {
+            username: 'oauth2',
+            password: token,
+          },
           envConfig: [
             {
               key: `http.${repo.host}/.extraheader`,
